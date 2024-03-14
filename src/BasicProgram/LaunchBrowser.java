dbc74cfb0687663f8382018f3d3c21dedd537bad
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-14 14:24:48 +0530

    adding selenium codes

---
+++ src/BasicProgram/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package BasicProgram;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.com/");
+		
+		driver.close();
+		
+		
+
+	}
+
+}
