ca1fe3f1412243201724978e16182f96d2c35a0b
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-15 13:37:10 +0530

    adding codes

---
+++ src/BasicProgram/LaunchBroedge.java
@@ -0,0 +1,19 @@
+package BasicProgram;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchBroedge {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		EdgeDriver driver = new EdgeDriver();
+		driver.get("https://www.w3schools.com/");
+		driver.findElement(By.id("search2")).sendKeys("HTML");
+		driver.findElement(By.id("learntocode_searchbtn")).click();
+		Thread.sleep(3000);
+
+	}
+
+}
